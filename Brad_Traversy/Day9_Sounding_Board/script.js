117354c9d5307716f95fe941f05768f39a757591
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-09-15 19:24:38 -0500

    50 Projects in 50 Days - Day 9 Project - Sounding Board
    Code for the day 9 project, a sounding board.

---
+++ Brad_Traversy/Day9_Sounding_Board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs () {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+    // song.pause and setting current time to 0 will stop a song.
+    song.pause();
+    song.currentTime = 0;
+  });
+}
