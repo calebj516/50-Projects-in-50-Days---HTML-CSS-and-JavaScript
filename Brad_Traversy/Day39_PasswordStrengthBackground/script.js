0989de5415e80d406442214689ba8dfe01c56a71
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-06 15:18:06 -0600

    Password Strength Background - Enhancements
    I made the following modifications to Brad's project:
    
    1. Used JS to obtain the pixels used in the blur function for the filter property on the background element in style.css
    2. Modified the JS to require a password with a minimum length of 8 characters, at least one special character, at least one number, and at least one uppercase letter.

--- Brad_Traversy/Day39_PasswordStrengthBackground/script.js
+++ Brad_Traversy/Day39_PasswordStrengthBackground/script.js
@@ -2,10 +2,58 @@
 
 const password = document.getElementById('password');
 const background = document.getElementById('background');
+// pixels is assigned the number of pixels used in the blur function on background element (see style.css)
+// I used this article for assistance: https://zellwk.com/blog/css-values-in-js/
+const pixels = getComputedStyle(background).filter.substring(5, 7);
 
 password.addEventListener('input', (e) => {
   const input = e.target.value;
   const length = input.length;
-  const blurValue = 20 - length * 2;
+  // minimum length of 8 is required, along with one special character, one number, and one uppercase character
+  const blurValue = pixels - ((length < 8 ? 0 : pixels / 4) + hasSpecialChar(input) + hasNumber(input) + hasUpperCase(input));
   background.style.filter = `blur(${blurValue}px)`;
 });
+
+// Has special character !@#$%^&*()
+function hasSpecialChar(input) {
+
+  let blurValue = 0;
+
+  if(input.match(/[!@#$%^&*()]/g)){
+    // blur value: 1/4 of pixels
+    blurValue = pixels / 4;
+  }
+
+  return blurValue;
+}
+
+
+
+// Has number
+function hasNumber(input) {
+
+  let blurValue = 0;
+
+  if(input.match(/\d/g)){
+    // blur value: 1/4 of pixels
+    blurValue = pixels / 4;
+  }
+
+  return blurValue;
+}
+
+
+
+// Has uppercase letter
+function hasUpperCase(input) {
+
+  let blurValue = 0;
+
+  if(input.match(/[A-Z]/g)){
+    // blur value: 1/4 of pixels
+    blurValue = pixels / 4;
+  }
+
+  return blurValue;
+}
+
